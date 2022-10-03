963afad2b68763a9a42f7342e50f1412d6da4caf
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2022-10-03 11:27:23 +0530

    Updated

--- fontend/src/components/AddVehicleType/index.jsx
+++ fontend/src/components/AddVehicleType/index.jsx
@@ -4,6 +4,8 @@ import {styleSheet} from "./styles";
 import {withStyles} from "@mui/styles";
 import {TextValidator, ValidatorForm} from "react-material-ui-form-validator";
 import CommonButton from "../common/Button";
+import VehicleTypeService from "../../services/VehicleTypeService";
+import CustomSnackBar from "../common/SnakBar";
 
 
 class AddVehicleType extends Component {
@@ -11,28 +13,72 @@ class AddVehicleType extends Component {
         super(props);
         this.state = {
             formData: {
-                userName: "", password: "",
+                vehicle_Type_Id: props.isUpdate?props.typeObj.vehicle_Type_Id:'',
+                type: props.isUpdate?props.typeObj.type:'',
+                loss_Damage_Waiver:props.isUpdate?props.typeObj.loss_Damage_Waiver:'',
             },
+            alert:false,
+            message:'',
+            severity:''
         };
     }
 
     handleSubmit = async () => {
-        console.log("Hi handle");
-        console.log(this.state.formData);
+        let formData = this.state.formData
+        if (this.props.isUpdate){
+            let res = await VehicleTypeService.updateVehicleType(formData)
+            if (res.status === 200) {
+                this.setState({
+                    alert:true,
+                    message:'Vehicle Type Updated!',
+                    severity:'success'
+                })
+
+            }else {
+                this.setState({
+                    alert:false,
+                    message:'Vehicle Type Update Unsuccessful!',
+                    severity:'error'
+                })
+            }
+
+        }else {
+            let res = await VehicleTypeService.postVehicleType(formData)
+            if (res.status === 201) {
+                this.setState({
+                    alert:true,
+                    message:'Vehicle Type Saved!',
+                    severity:'success'
+                })
+
+            }else {
+                this.setState({
+                    alert:false,
+                    message:'Vehicle Type Saved Unsuccessful!',
+                    severity:'error'
+                })
+            }
+        }
+
     };
 
     handleChange = (event) => {
         let id = event.target.name;
         switch (id) {
-            case "userName":
-                const userName = event.target.value;
-                this.setState(Object.assign(this.state.formData, {userName: userName}));
+            case "vehicle_Type_Id":
+                const vehicle_Type_Id = event.target.value;
+                this.setState(Object.assign(this.state.formData, {vehicle_Type_Id: vehicle_Type_Id}));
                 // this.setState({ userId });
                 break;
 
-            case "password":
-                const password = event.target.value;
-                this.setState(Object.assign(this.state.formData, {password: password}));
+            case "type":
+                const type = event.target.value;
+                this.setState(Object.assign(this.state.formData, {type: type}));
+                break;
+
+            case "loss_Damage_Waiver":
+                const loss_Damage_Waiver = event.target.value;
+                this.setState(Object.assign(this.state.formData, {loss_Damage_Waiver: loss_Damage_Waiver}));
                 break;
 
             default:
@@ -42,64 +88,76 @@ class AddVehicleType extends Component {
 
     render() {
         const {classes} = this.props;
-        return (<Grid container direction={'row'} xs={12} className={classes.container}>
-            <ValidatorForm
-                onSubmit={this.handleSubmit}
-                onError={(errors) => console.log(errors)}
-                className={classes.validator}
-            >
-                <Grid item container direction={'row'} xs={12} gap={'15px'} justifyContent={'center'}
-                      alignContent={'center'}
-                      className={classes.container}>
-                    <Grid item container direction={'column'} xs={12} gap={'15px'}>
-                        <TextValidator
-                            label="Vehicle Type ID"
-                            onChange={this.handleChange}
-                            name="password"
-                            value={this.state.formData.password}
-                            validators={["required"]}
-                            errorMessages={["This field is required"]}
-                            type={"password"}
-                            className="w-full"
-                            style={{minWidth: '100%'}}
-                        />
-                        <TextValidator
-                            label="Type"
-                            onChange={this.handleChange}
-                            name="password"
-                            value={this.state.formData.password}
-                            validators={["required"]}
-                            errorMessages={["This field is required"]}
-                            type={"password"}
-                            className="w-full"
-                            style={{minWidth: '100%'}}
-                        />
-                        <TextValidator
-                            label="L.D.W"
-                            onChange={this.handleChange}
-                            name="password"
-                            value={this.state.formData.password}
-                            validators={["required"]}
-                            errorMessages={["This field is required"]}
-                            type={"password"}
-                            className="w-full"
-                            style={{minWidth: '100%'}}
-                        />
-                        <CommonButton
-                            size="large"
-                            variant="contained"
-                            label="Add"
-                            type="submit"
-                            className="text-white bg-blue-500 font-bold tracking-wide"
-                            style={{backgroundColor: 'rgba(25, 118, 210, 0.95)', width: '100%'}}
-                        />
-                    </Grid>
+        return (
+
+            <>
+                <Grid container direction={'row'} xs={12} className={classes.container}>
+                    <ValidatorForm
+                        onSubmit={this.handleSubmit}
+                        onError={(errors) => console.log(errors)}
+                        className={classes.validator}
+                    >
+                        <Grid item container direction={'row'} xs={12} gap={'15px'} justifyContent={'center'}
+                              alignContent={'center'}
+                              className={classes.container}>
+                            <Grid item container direction={'column'} xs={12} gap={'15px'}>
+                                <TextValidator
+                                    label="Vehicle Type ID"
+                                    onChange={this.handleChange}
+                                    name="vehicle_Type_Id"
+                                    value={this.state.formData.vehicle_Type_Id}
+                                    validators={["required"]}
+                                    errorMessages={["This field is required"]}
+                                    className="w-full"
+                                    style={{minWidth: '100%'}}
+                                />
+                                <TextValidator
+                                    label="Type"
+                                    onChange={this.handleChange}
+                                    name="type"
+                                    value={this.state.formData.type}
+                                    validators={["required"]}
+                                    errorMessages={["This field is required"]}
+                                    className="w-full"
+                                    style={{minWidth: '100%'}}
+                                />
+                                <TextValidator
+                                    label="L.D.W"
+                                    onChange={this.handleChange}
+                                    name="loss_Damage_Waiver"
+                                    value={this.state.formData.loss_Damage_Waiver}
+                                    validators={["required"]}
+                                    errorMessages={["This field is required"]}
+                                    className="w-full"
+                                    style={{minWidth: '100%'}}
+                                />
+                                <CommonButton
+                                    size="large"
+                                    variant="contained"
+                                    label="Add"
+                                    type="submit"
+                                    className="text-white bg-blue-500 font-bold tracking-wide"
+                                    style={{backgroundColor: 'rgba(25, 118, 210, 0.95)', width: '100%'}}
+                                />
+                            </Grid>
+
+                        </Grid>
+                    </ValidatorForm>
 
                 </Grid>
-            </ValidatorForm>
-
-        </Grid>);
+                <CustomSnackBar
+                    open={this.state.alert}
+                    onClose={() => {
+                        this.setState({alert: false})
+                    }}
+                    message={this.state.message}
+                    autoHideDuration={3000}
+                    severity={this.state.severity}
+                    variant={'filled'}
+                />
+            </>);
     }
+
 }
 
 export default withStyles(styleSheet)(AddVehicleType);
--- fontend/src/pages/Admin/Vehicle Types/index.jsx
+++ fontend/src/pages/Admin/Vehicle Types/index.jsx
@@ -1,4 +1,4 @@
-import {Dialog, DialogContent, DialogTitle, Grid, IconButton, Typography,} from "@mui/material";
+import {Dialog, DialogContent, DialogTitle, Grid, IconButton, Tooltip, Typography,} from "@mui/material";
 import React, {Component} from "react";
 import Navbar from "../../../components/common/Navbar/Admin";
 import Sidebar from "../../../components/common/Sidebar";
@@ -9,6 +9,10 @@ import {withStyles} from "@mui/styles";
 import {styleSheet} from "./styles";
 import CloseIcon from "@mui/icons-material/Close";
 import AddVehicleType from "../../../components/AddVehicleType";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VehicleTypeService from "../../../services/VehicleTypeService";
+import CustomSnackBar from "../../../components/common/SnakBar";
 
 class VehicleType extends Component {
     constructor(props) {
@@ -19,6 +23,9 @@ class VehicleType extends Component {
             message: "",
             severity: "",
 
+            isUpdate: false,
+            updateVehicleType:{},
+
             //  for table
             data: [],
             loaded: false,
@@ -26,19 +33,19 @@ class VehicleType extends Component {
             //  for data table
             columns: [
                 {
-                    field: "registrationNumber",
+                    field: "vehicle_Type_Id",
                     headerName: "Vehicle Type ID",
                     width: 400,
                 },
 
                 {
-                    field: "Type",
+                    field: "type",
                     headerName: "Type",
                     width: 400,
                 },
 
                 {
-                    field: "L.D.W.",
+                    field: "loss_Damage_Waiver",
                     headerName: "L.D.W.",
                     width: 400,
                     sortable: false,
@@ -48,20 +55,88 @@ class VehicleType extends Component {
                     field: "Action",
                     headerName: "Action",
                     width: 400,
+                    renderCell: (params) => {
+                        return (
+                            <>
+                                <Tooltip title="Edit">
+                                    <IconButton onClick={async () => {
+                                        await this.updateVehicleType(params.row);
+                                    }}>
+                                        <EditIcon className={'text-blue-500'}/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Delete">
+                                    <IconButton onClick={async () => {
+                                        await this.deleteVehicleType(params.row.vehicle_Type_Id);
+                                    }}>
+                                        <DeleteIcon className={'text-red-500'}/>
+                                    </IconButton>
+                                </Tooltip>
+                            </>
+                        )
+                    }
                 },
             ],
         };
     }
 
-    async loadData() {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.popup == true){
+            this.loadVtypeData()
+        }
+    }
+
+    deleteVehicleType = async (id) => {
+        let params = {
+            id: id
+        }
+        let res = await VehicleTypeService.deleteVehicleType(params)
+        if (res.status === 200){
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'success'
+            })
+            this.loadVtypeData()
+        }else {
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'error'
+            })
+        }
+        this.loadVtypeData()
+    }
+
+    updateVehicleType = async (data) => {
+        const row = data;
+        let updateVehicleType = {
+            "vehicle_Type_Id": row.vehicle_Type_Id,
+            "type": row.type,
+            "loss_Damage_Waiver": row.loss_Damage_Waiver,
+        }
+        await this.setState({updateVehicleType: updateVehicleType})
+        await this.setState({
+            popup: true,
+            isUpdate: true
+        })
+    }
+
+    async loadVtypeData() {
         // let resp = await PostService.fetchPosts();
-        const data = [];
-        this.setState({
-            loaded: true,
-            data: data,
-        });
-        console.log(this.state.data);
-        // console.log(JSON.stringify(resp.data));
+        let resp = await VehicleTypeService.fetchVehicleType();
+        let nData = [];
+        if (resp.status === 200) {
+            resp.data.data.map((value, index) => {
+                value.id = value.vehicle_Type_Id;
+                nData.push(value)
+            })
+
+            this.setState({
+                loaded: true,
+                data: nData,
+            });
+        }
     }
 
     componentDidMount() {
@@ -133,9 +208,19 @@ class VehicleType extends Component {
                         </div>
                     </DialogTitle>
                     <DialogContent dividers>
-                        <AddVehicleType/>
+                        <AddVehicleType isUpdate={this.state.isUpdate} typeObj={this.state.updateVehicleType}/>
                     </DialogContent>
                 </Dialog>
+                <CustomSnackBar
+                    open={this.state.alert}
+                    onClose={() => {
+                        this.setState({alert: false})
+                    }}
+                    message={this.state.message}
+                    autoHideDuration={3000}
+                    severity={this.state.severity}
+                    variant={'filled'}
+                />
             </Grid>
         );
     }
